b0651e26b364128ea12e3de785ce2eacf9213ddf
identitymattersapparel-dev <identitymattersapparel@gmail.com>
2026-04-16 10:49:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useRef } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import StartScreen from "./components/StartScreen";
 import MainScreen from "./components/MainScreen";
 import {
